40290ee8ea05ef9cfbc53b209b1e3802d5f7e9de
Fatih <eem.fatih@outlook.com>
2019-12-31 13:00:51 +0300

    Add project files.

---
+++ Ticket.DAL/Migrations/20191231093335_AddNameSurname.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ticket.DAL.Migrations
+{
+    public partial class AddNameSurname : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Surname",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Surname",
+                table: "Users");
+        }
+    }
+}
---
+++ Ticket.DAL/TicketDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Ticket.Entities;
+
+namespace Ticket.DAL
+{
+    public class TicketDBContext:DbContext
+    {
+        public TicketDBContext(DbContextOptions<TicketDBContext> options):base(options)
+        {
+
+        }
+        public DbSet<User> Users{ get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+         
+        }
+    }
+}
