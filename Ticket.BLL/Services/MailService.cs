1b9870d1966b31b7a93323ce858cb2fe517acc07
Fatih <eem.fatih@outlook.com>
2020-01-07 14:10:19 +0300

    SendGridKeyChanged

--- Ticket.BLL/Services/MailService.cs
+++ Ticket.BLL/Services/MailService.cs
@@ -1,25 +1,42 @@
-﻿using SendGrid;
+﻿using Microsoft.Extensions.Configuration;
+using SendGrid;
 using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Ticket.BLL.IServices;
+using Ticket.Helpers;
 
 namespace Ticket.BLL.Services
 {
-    public class MailService:IMailService
+    public class MailService : IMailService
     {
+        IConfiguration _configuration;
+        public MailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public async Task SendEmail(string email, string subject, string htmlContent)
         {
-            var apiKey = "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";
-            var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("qr1903@gmail.com", "Support");
-            var to = new EmailAddress(email);
-            var plainTextContent = Regex.Replace(htmlContent, "<[^>]*>", "");
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            var response = await client.SendEmailAsync(msg);
+            try
+            {
+                var apiKey = _configuration.GetSection("SendGridAPIKey").Value;
+                var client = new SendGridClient(apiKey);
+                var from = new EmailAddress("qr1903@gmail.com", "Support");
+                var to = new EmailAddress(email);
+                var plainTextContent = Regex.Replace(htmlContent, "<[^>]*>", "");
+                var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+                var response = await client.SendEmailAsync(msg);
+            }
+            catch (Exception e)
+            {
+                throw new AppException(e.Message);
+
+            }
         }
     }
 }
